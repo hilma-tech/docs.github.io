6e9f2960699e88232e6efe8179ab461067ab996d
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-08-03 10:41:23 +0300

    Deploy website - based on 0b04a126c7eaa0f99d3000d1346d5aba4fd9fd08

--- siteConfig.js
+++
@@ -1,6 +0,0 @@
-const siteConfig = {
-	url: 'https://hilma-tech.github.io/docs.github.io', // Your website URL
-	// baseUrl: '/docs.github.io/',
-	projectName: 'docs',
-	organizationName: 'hilma-tech'
-  }
